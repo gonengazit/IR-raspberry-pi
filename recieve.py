7381f88103b7a77bd4beffce1aaa87e4b66c422b
gonengazit <32018445+gonengazit@users.noreply.github.com>
2017-09-16 18:53:51 +0300

    Add files via upload

---
+++ recieve.py
@@ -0,0 +1,192 @@
+import serial, time,random,struct,sys
+from lz4.frame import decompress,compress
+from contextlib import closing
+import numpy as np
+# import commpy.channelcoding.convcode as cc
+# from commpy.utilities import *
+from bitstring import BitArray
+from Queue import Queue
+import threading
+import socket
+
+ser = serial.Serial(port="/dev/serial0", baudrate=1151200)
+
+socket.setdefaulttimeout(3)
+
+def deinterlieve(bits,n):
+    cutoff=bits.size-(len(bits)%n)
+    return np.concatenate((bits[:cutoff].reshape(n,bits[:cutoff].size/n).transpose().flatten(), bits[cutoff:]))
+
+def deinterlieve2(a):
+    return a[::2],a[1::2]
+
+def rep_decode(bits,n):
+    bitsmat=np.sum(bits.reshape(bits.size/n,n),axis=1)>=2
+    return bitsmat.astype(int)
+
+def decode(byteData):
+    if byteData==b"":
+        return
+    # generator_matrix = np.array([[04, 05, 07]])
+    # M = np.array([3])
+    # trellis = cc.Trellis(M, generator_matrix)
+    # tb_depth = 5 * (M.sum() + 1)
+    # message_bits = deinterlieve(np.array(map(int, dataformats.bin))[7:],3)
+    # decoded_bits = cc.viterbi_decode(message_bits.astype(float), trellis, tb_depth)[:-M]
+    # print(len(bytesNew))
+    # byteData=decompress(bytesNew)
+
+
+    dataformats = BitArray(hex=byteData.encode("hex"))
+    message_bits = deinterlieve(np.array(map(int, dataformats.bin)), 3)
+    decoded_bits = rep_decode(message_bits, 3)
+    newMessage = "".join(map(str,decoded_bits))
+    bytesNew = b""
+    for i in xrange(0, len(newMessage), 8):
+        bytesNew += (struct.pack("B", int(newMessage[i:i + 8],2)))
+    return bytesNew
+
+def check(payload):
+    random.seed("gonen")
+    if payload==b"".join(struct.pack("B",random.randrange(16)) for _ in xrange(20)):
+        # print("heartbeat recieved")
+        return False
+    # random.seed("gonen")
+    # #data = b"".join(struct.pack("B",random.randrange(16)) for _ in range(len(payload)))
+    # data=b"".join(struct.pack("B",i) for i in range(len(payload)))
+    # print ("data length: %d"%(len(payload)))
+    # print(payload == data)
+    else:
+        print(payload.decode("utf-8"))
+        return True
+class reciever(object):
+    def __init__(self, frmsize, frmsync):
+        self.buffer = b""
+        self.framesize = frmsize
+        self.framesync=struct.pack("I",int(frmsync,2))[1:]
+        self.packetnumber=struct.pack("B",0)
+    def recieve(self,counterqueue,tcpQueue):
+        idx=0
+        mode="framesync"
+        pcktsize=0
+        while True:
+            if mode=="framesync":
+                while idx!=len(self.framesync):
+                    a=ser.read(1)
+                    if a==self.framesync[idx]:
+                        idx+=1
+                    else:
+                        if a==self.framesync[0]:
+                            idx=1
+                        else:
+                            idx=0
+                idx=0
+                mode = "message"
+            elif mode=="message":
+                message=ser.read(self.framesize*2)
+                header,payload=deinterlieve2(message)
+                decoded_header=decode(header)
+                pcktnum=decoded_header[0]
+                if pcktnum!=self.packetnumber:
+
+                    #decode the buffer to see if you have enough info to make a messege
+                    # decode(self.buffer,pcktsize)
+                    self.packetnumber=pcktnum
+                    self.buffer=b""
+                pcktsize = struct.unpack("H",decoded_header[1:3])[0]
+                offset   = struct.unpack("H",decoded_header[3:5])[0]
+                self.buffer+=struct.pack("B",0)*(offset-len(self.buffer))
+                self.buffer+=payload
+                if len(self.buffer)>=pcktsize:
+                    # print(len(self.buffer),pcktsize*2)
+                    # check(decompress(decode(self.buffer[:pcktsize])))
+                    try:
+                        finalData=decompress(decode(self.buffer[:pcktsize]))
+                    except:
+                        print("error %s"%sys.exc_info()[0])
+                    else:
+                        counterqueue.get()
+                        counterqueue.put(time.time())
+                        if check(finalData):
+                            tcpQueue.put(finalData)
+                    finally:
+                        self.buffer=b""
+                        self.packetnumber = b"-1"
+                mode="framesync"
+
+def HB(counterqueue):
+    while True:
+        gcounter=counterqueue.get()
+        counterqueue.put(gcounter)
+        if time.time()-gcounter>5:
+            print("error, no heartbeat recieved %d"%(time.time()-gcounter))
+
+        time.sleep(.1)
+
+def send(tcpQueue):
+    TCP_IP = '20.0.0.3'
+    TCP_PORT = 8000
+    BUFFER_SIZE = 4096
+    counter=0
+    while True:
+        MESSAGE=tcpQueue.get()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # MESSAGE = b"GET /foo2.html HTTP/1.1\r\nHost: 192.168.1.14:8000\r\nConnection: keep-alive\r\nCache-Control: max-age=0\r\nUpgrade-Insecure-Requests: 1\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8\r\nAccept-Encoding: gzip, deflate, br\r\nAccept-Language: en-US,en;q=0.8,he;q=0.6\r\nIf-Modified-Since: Fri, 08 Sep 2017 22:46:30 GMT\r\n\r\n"
+
+        for i in xrange(3):
+            try:
+                s.connect((TCP_IP, TCP_PORT))
+            except (socket.error,socket.timeout) as e:
+                print("error")
+                print(str(e))
+            else:
+                break
+        else:
+            s.close()
+            continue
+        with closing(s):
+            for i in xrange(3):
+                try:
+                    s.send(MESSAGE)
+
+                    data=s.recv(BUFFER_SIZE)
+                except socket.timeout as e:
+                    print(str(e))
+                else:
+                    counter += 1
+                    print(counter)
+                    if data[9:17]==b"200 OK\r\n":
+                        print("OK recieved")
+                        print(data.decode("utf-8"))
+                        break
+                    else:
+                        print("no OK")
+                        print(data.decode("utf-8"))
+            else:
+                pass
+                #flash  red led
+    # s.close()
+
+
+a=reciever(15,"01"*4+"0011"*2+"11"*4)
+# a.recieve(time.time())
+
+globalqueue=Queue()
+tcpQueue=Queue()
+globalqueue.put(time.time())
+recieverThread=threading.Thread(target=a.recieve,args=(globalqueue,tcpQueue))
+HBThread=threading.Thread(target=HB,args=(globalqueue,))
+tcpThread=threading.Thread(target=send,args=(tcpQueue,))
+
+recieverThread.start()
+HBThread.start()
+tcpThread.start()
+
+
+# while True:
+# 	a=ser.in_waiting
+# 	if a:
+# 		b = ser.read(a)
+# 		print(a)
+# 		print(b)
+# 	time.sleep(1)
---
+++ transmit.py
@@ -0,0 +1,109 @@
+import serial,time,string,random,struct,datetime,pytz
+from lz4.frame import compress,decompress
+import numpy as np
+from numpy.matlib import repmat
+from contextlib import closing
+
+# from commpy.channelcoding.convcode import Trellis,conv_encode,viterbi_decode
+# from commpy.utilities import *
+from bitstring import BitArray
+import socket
+ser = serial.Serial(port="/dev/serial0", baudrate=1151200)
+#ser.flushInput()
+#ser.flushOutput()
+
+def interlieve(bits,n):
+    cutoff=bits.size-(len(bits)%n)
+    return np.concatenate((bits[:cutoff].reshape(cutoff / n, n).transpose().flatten(), bits[cutoff:]))
+
+def interlieve2(a1,a2):
+    return b"".join(j for i in zip(a1,a2) for j in i)
+
+def rep_encode(bits,n):
+    return np.repeat(bits,n)
+
+def encode(data):
+    compressedData = data
+    # print(len(compressedData))
+    # generator_matrix = np.array([[04, 05, 07]])
+    # M = np.array([3])
+    # trellis = Trellis(M, generator_matrix)
+    # coded_bits = interlieve(conv_encode(message_bits, trellis),3)
+    # newMessage="0"*((8-len(coded_bits)%8)%8)+ "".join(map(str,coded_bits))
+    # thing =  np.array(map(int, newMessage))[4:]
+    # print(np.array_equal(message_bits, viterbi_decode(thing, trellis, 5 * (M.sum() + 1))[:-M]))
+
+
+    dataformats=BitArray(hex=compressedData.encode("hex"))
+    message_bits=np.array(map(int,dataformats.bin))
+    coded_bits = interlieve(rep_encode(message_bits, 3), 3)
+    newMessage = "".join(map(str,coded_bits))
+    encodedNew=b""
+    for i in xrange(0,len(newMessage),8):
+        encodedNew+=(struct.pack("B",int(newMessage[i:i+8],2)))
+    return encodedNew
+
+class fragmenter(object):
+    def __init__(self,framesize,framesync):
+        self.framesize=framesize
+        self.framesync=int(framesync,2)
+        self.packetnumber=0
+    def fragment(self,data):
+        pcktsize = len(data)
+        data+=struct.pack("B",0)*((self.framesize-len(data)%self.framesize)%self.framesize)
+        newdata=  [struct.pack("I",self.framesync)[1:]
+                  +interlieve2(encode(
+                   struct.pack("B",self.packetnumber)
+                  +struct.pack("H",pcktsize)
+                  +struct.pack("H",i)
+                  )
+                  ,data[i:i+self.framesize]) for i in xrange(0,len(data),self.framesize)]
+        self.packetnumber =(self.packetnumber+1)%256
+        # print([(len(i),self.framesize+8) for i in newdata ])
+        return newdata
+#all bytes of framesync must be unique
+a=fragmenter(15,"01"*4+"0011"*2+"11"*4)
+socket.setdefaulttimeout(1)
+HOST = '0.0.0.0'    # The remote host
+PORT = 80
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen(1)
+# msglen=255
+
+# data=b"".join(struct.pack("B",i) for i in range(msglen))
+ID=1754122158
+dateFormat="%a, %d %b %Y %H:%M:%S %Z"
+HTTPrequest=b"HTTP/1.1 200 OK\r\nCache-Control: no-cache, no-store\r\nPragma: no-cache\r\nContent-Length: 30\r\nContent-Type: text/html; charset=iso-8859-8\r\nExpires: -1\r\nServer: Microsoft-IIS/8.5\r\nX-AspNet-Version: 4.0.30319\r\nSet-Cookie: ASP.NET_SessionId=5jxc42k1m04pa30weih5y43s; path=/; HttpOnly\r\nX-Powered-By: ASP.NET\r\nDate: {date:%a, %d %b %Y %H:%M:%S %Z}\r\n\r\n0: Message sent, ID={id}"
+
+with closing(s):
+    while True:
+        heartbeat=False
+        try:
+            conn, addr = s.accept()
+            print("accepted")
+            data = conn.recv(2048)
+        except socket.timeout:
+            random.seed("gonen")
+            data=b"".join(struct.pack("B",random.randrange(16)) for _ in xrange(20))
+            heartbeat=True
+        else:
+            try:
+                conn.send(HTTPrequest.format(date=datetime.datetime.now(pytz.timezone("GMT")),id=ID))
+            except (socket.error,socket.timeout) as e:
+                print(str(e))
+            else:
+                ID+=1
+            finally:
+                conn.close()
+        if data:
+            compressedData=compress(data, content_checksum=1)
+            encodedData=encode(compressedData)
+            # encodedData=encode(data)
+
+            for i in a.fragment(encodedData):
+                ser.write(i)
+            if not heartbeat:
+                print("sent messege of %d bits"%len(compressedData))
+# while True:
+#     ser.write(raw_input("what do you what to transmit?   "))
